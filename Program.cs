d4e67b0696c76baa0b7aa68e13956d3ac227de6c
Matthew Burke <matthew.burke@cantab.net>
2017-03-06 22:04:36 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(
+                context => {
+                    return context.Response.WriteAsync("Hello World!!");
+                    }
+            );
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
