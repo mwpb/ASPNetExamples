67ac0ecbe1affe5edc6b3adfbe73ac97dc1f0289
Matthew Burke <matthewpatrickburke@gmail.com>
2017-03-08 21:40:58 +0000

    Config file and local library.

--- Lesson2/Lesson2/src/Lesson2/Startup.cs
+++ Lesson2/Lesson2/src/Lesson2/Startup.cs
@@ -7,11 +7,20 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace Lesson2
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -36,7 +45,11 @@ namespace Lesson2
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync($"This is in {env.EnvironmentName}");
+                await context.Response.WriteAsync(
+                    $"This is in {env.EnvironmentName}\n"+
+                    $"with config: {Configuration["Message"]}\n"+
+                    $"Stuff from the library: {testLib.Class1.Greeting()}"
+                    );
             });
 
         }
